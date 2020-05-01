4b559d64657b48a2d1741668fa3e6af058fd6329
Armen G <gharibi.armen@yahoo.com>
2020-04-30 18:42:50 -0700

    Added project status data to the pie-chart of the UI

--- src/main/java/com/agharibi/projectmanagement/controllers/HomeController.java
+++ src/main/java/com/agharibi/projectmanagement/controllers/HomeController.java
@@ -2,14 +2,19 @@ package com.agharibi.projectmanagement.controllers;
 
 import com.agharibi.projectmanagement.dao.EmployeeRepository;
 import com.agharibi.projectmanagement.dao.ProjectRepository;
-import com.agharibi.projectmanagement.entities.Employee;
+import com.agharibi.projectmanagement.dto.EmployeeProject;
+import com.agharibi.projectmanagement.dto.ChartData;
 import com.agharibi.projectmanagement.entities.Project;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -21,12 +26,20 @@ public class HomeController {
     EmployeeRepository employeeRepository;
 
     @GetMapping("/")
-    public String displayHome(Model model) {
+    public String displayHome(Model model) throws Exception {
+
+        Map<String, Object> map = new HashMap<>();
+
         List<Project> projects = projectRepository.findAll();
         model.addAttribute("projects", projects);
 
-        List<Employee> employees = employeeRepository.findAll();
-        model.addAttribute("employees", employees);
+        List<ChartData> chartData = projectRepository.getProjectStatus();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper.writeValueAsString(chartData);
+        model.addAttribute("projectStatusCount", jsonString);
+
+        List<EmployeeProject> employeeProjects = employeeRepository.employeeProjects();
+        model.addAttribute("employeeProjects", employeeProjects);
 
         return "main/home";
     }
---
+++ src/main/java/com/agharibi/projectmanagement/dto/EmployeeProject.java
@@ -0,0 +1,9 @@
+package com.agharibi.projectmanagement.dto;
+
+public interface EmployeeProject {
+
+    String getFirstName();
+    String getLastName();
+    int getProjectCount();
+
+}
